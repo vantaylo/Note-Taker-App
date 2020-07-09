acafdbd2db80f2e8599d1d6f8cfa233dab4186ce
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-07-09 14:37:15 -0500

    created server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 //require the modules installed and then call them
 const express = require("express");
+
+//create server
+var app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
