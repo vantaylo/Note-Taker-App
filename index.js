ee7d069f5935e7f4b53dda18bc851ca684032bc2
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-07-09 14:52:38 -0500

    created routes to homepage and notes page

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 //require the modules installed and then call them
 const express = require("express");
+const path = require("path");
 
 //create server
-var app = express();
+const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+//routing
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
