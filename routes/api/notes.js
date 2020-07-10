7bf99eab4fa97009604d8d69345d6883bfee9569
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-07-10 11:15:17 -0500

    router endpoint for fetching and writing notes

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,11 +1,29 @@
 const express = require("express");
-const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 
-//get data file
+const router = express.Router();
 const dataFile = path.join(__dirname, "./../../db/db.json");
 
-//read db.json
+router.get("/", function (req, res) {
+  fs.readFile(dataFile, (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
 
-//return all saved notes
+router.post("/", function (req, res) {
+  let title = req.body.title;
+  let text = req.body.text;
+  console.log("My note " + title + " " + text);
+
+  fs.readFile(dataFile, (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes.push({ title, text });
+    console.log(notes);
+  });
+});
+
+module.exports = router;
